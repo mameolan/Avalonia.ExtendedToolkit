a0d6ecf71b782cd2a9a206894981f78aed04fc0b
mclord <mclord76@yahoo.de>
2019-10-25 05:56:50 +0200

    add calendarstyle some how color is not updating

---
+++ Avalonia.ExampleApp/Views/DateExamples.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Avalonia.ExampleApp.Views
+{
+    public class DateExamples : UserControl
+    {
+        public DateExamples()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
