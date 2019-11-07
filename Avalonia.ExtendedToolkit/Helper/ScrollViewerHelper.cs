ac289851e5b2e6e1e89feebb817e7dd29e2350bc
mclord <mclord76@yahoo.de>
2019-11-07 07:24:40 +0100

    try to fix scrollbar add unfinished ScrollViewer

---
+++ Avalonia.ExtendedToolkit/Helper/ScrollViewerHelper.cs
@@ -0,0 +1,28 @@
+﻿using Avalonia.Controls;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Avalonia.ExtendedToolkit.Controls
+{
+    public static class ScrollViewerHelper
+    {
+
+
+        public static readonly AttachedProperty<bool> VerticalScrollBarOnLeftSideProperty =
+            AvaloniaProperty.RegisterAttached<IControl, bool>
+            ("VerticalScrollBarOnLeftSide", typeof(ScrollViewerHelper));
+
+        public static bool GetVerticalScrollBarOnLeftSide(IControl element)
+        {
+            return element.GetValue(VerticalScrollBarOnLeftSideProperty);
+        }
+
+        public static void SetVerticalScrollBarOnLeftSide(IControl element, bool value)
+        {
+            element.SetValue(VerticalScrollBarOnLeftSideProperty, value);
+        }
+
+
+    }
+}
