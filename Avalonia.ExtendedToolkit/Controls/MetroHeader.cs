966a6be4bb62f495834808c30e8fb54b53b603f4
mclord <mclord76@yahoo.de>
2019-11-09 14:18:26 +0100

    add MetroHeader,style and example

---
+++ Avalonia.ExtendedToolkit/Controls/MetroHeader.cs
@@ -0,0 +1,16 @@
+﻿using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Avalonia.ExtendedToolkit.Controls
+{
+    public class MetroHeader : GroupBox, IStyledElement
+    {
+        public MetroHeader()
+        {
+            
+        }
+    }
+}
