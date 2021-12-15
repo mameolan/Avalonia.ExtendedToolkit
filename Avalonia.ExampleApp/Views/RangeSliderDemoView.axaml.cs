235033fe9f9b79f185220261958e22f57540e338
mameolan <mameolan2@yahoo.de>
2021-12-15 07:37:00 +0100

    add RangeSlider

---
+++ Avalonia.ExampleApp/Views/RangeSliderDemoView.axaml.cs
@@ -0,0 +1,22 @@
+using System;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Avalonia.ExampleApp.Views
+{
+    public partial class RangeSliderDemoView : UserControl
+    {
+        public RangeSliderDemoView()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        
+    }
+}
